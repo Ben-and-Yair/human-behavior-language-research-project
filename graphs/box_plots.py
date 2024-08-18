10446504edd21a022571b4adc3502d25811c6682
YairSlobodin <139879854+YairSlobodin@users.noreply.github.com>
2024-08-18 17:47:01 +0300

    add box_blot.py

---
+++ graphs/box_plots.py
@@ -0,0 +1,48 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# user input
+def input_params():
+    csv_path = '../csvFiles/filtered_recordings_Olly.csv'
+    features = ['SESS', 'Reaction_time(s)', 'Total_duration(s)'] # do NOT remove SESS from the list
+    sess = [1, 4]  # SESSions to plot
+    should_normalize = True
+
+    return csv_path, features, sess, should_normalize
+
+
+def normalize_df(df, should_normalize):
+    if should_normalize:
+        for column in list(df.columns):
+            if column != "SESS":
+                df[column] = (df[column] - df[column].mean()) / df[column].mean()
+    return df
+
+
+def plot_feature_boxplot(df, title):
+    sns.set(style="ticks")
+    fig, ax = plt.subplots(1, figsize=(10, 10))
+    df_melted = pd.melt(df, id_vars=['SESS'], value_vars=list(df.columns))
+    p = sns.boxplot(x="variable", y="value", hue='SESS', data=df_melted, palette="Pastel1", showfliers=False)
+    labels = [label.get_text() for label in ax.get_xticklabels()]
+    ax.set_xticklabels([label[::-1] if not label.isascii() else label for label in labels])
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.2)
+    p.set_title(title)
+    plt.show()
+
+
+def main():
+    csv_file, features, sess, should_normalize = input_params()
+    df = pd.read_csv(csv_file)
+    df = df[features]
+    df = df[df['SESS'].isin(sess)]
+
+    df = normalize_df(df, should_normalize)
+    plot_feature_boxplot(df, "features box plots")
+
+
+if __name__ == "__main__":
+    main()
